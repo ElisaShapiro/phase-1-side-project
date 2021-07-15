2dcb4a992276223fbb6bf178a586ad933085739e
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-15 10:47:40 -0400

    reveal button working

--- script.js
+++ script.js
@@ -12,14 +12,23 @@ function oneJoke(joke){
     joke.forEach(joke => {
         let divContainer = document.createElement("div")
         let h4Setup = document.createElement("h4")
-        // let pPunchline = document.createElement("p")
+        let pPunchline = document.createElement("p")
+        let btnPunchline = document.createElement("button")
         divContainer.id = "jokeContainerDiv"
         h4Setup.id = "setupLine"
-        // pPunchline.id = "punchlineLine"
+        pPunchline.id = "punchlineLine"
+        btnPunchline.id = "toggle"
         h4Setup.textContent = joke.setup
-        // pPunchline.textContent = joke.punchline
-        
-        divContainer.append(h4Setup, pPunchline)
+        btnPunchline.textContent = "Reveal Punchline"
+        divContainer.append(h4Setup, btnPunchline, pPunchline)
+        btnPunchline.addEventListener('click', (e) => {
+            pPunchline.textContent = joke.punchline
+            if (pPunchline.style.display !== "none") {
+                pPunchline.style.display = "none";
+            } else {
+                pPunchline.style.display = "block";
+            }
+        })
         document.querySelector("#joke-container").append(divContainer)
     })
 }
