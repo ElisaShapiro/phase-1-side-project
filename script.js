72cd50a291f2f0578a6e7be985e6072df4f8e4a4
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-15 10:25:03 -0400

    render 10 jokes

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    fetchJokes()
 })
+
+function fetchJokes(){
+    fetch('https://official-joke-api.appspot.com/random_ten')
+    .then(res => res.json())
+    .then(data => oneJoke(data))
+}
+
+function oneJoke(joke){
+    joke.forEach(joke => {
+        let divContainer = document.createElement("div")
+        let h4Setup = document.createElement("h4")
+        // let pPunchline = document.createElement("p")
+        divContainer.id = "jokeContainerDiv"
+        h4Setup.id = "setupLine"
+        // pPunchline.id = "punchlineLine"
+        h4Setup.textContent = joke.setup
+        // pPunchline.textContent = joke.punchline
+        
+        divContainer.append(h4Setup, pPunchline)
+        document.querySelector("#joke-container").append(divContainer)
+    })
+}
